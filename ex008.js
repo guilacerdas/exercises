3aa81701d803396fd7815c310bb117a0f57f8c6a
guilacerdas <98661584+guilacerdas@users.noreply.github.com>
2022-06-15 10:18:22 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 150
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirige sempre com cinto de segurança')
